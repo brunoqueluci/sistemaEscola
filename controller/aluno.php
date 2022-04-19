273947ad00b237c70525b563a6ed3bf943ccef75
DESKTOP-T0LU4IO\User <brunoqueluci@gmail.com>
2022-04-19 16:42:39 -0300

    commit inicial. Deploy

---
+++ controller/aluno.php
@@ -0,0 +1,41 @@
+<?php
+include_once("../model/conexao.php");
+include_once("../model/cadastrar.php");
+
+function dados_aluno()
+{
+    $dados_aluno = array 
+    (
+        'nome' => $_POST['nome'],
+        'matricula' => $_POST['matricula'],
+        'data_nasc' => $_POST['data_nasc'],
+        'idade' => calcula_idade( $_POST['data_nasc'])
+    );
+
+    return $dados_aluno;
+};
+
+function cadastrar(){
+    $dados = dados_aluno();
+    $id = insert($dados);
+}
+
+cadastrar();
+
+function calcula_idade($data){
+
+    $data_atual = date("Y-m-d");
+    $lista = explode('-', $data_atual);
+    
+    $ano_atual = $lista[0];
+    $mes_atual = $lista[1];
+
+    $lista_dt_nasc = explode('-', $data);
+    
+    $ano_nasc = $lista_dt_nasc[0];
+    $mes_nasc = $lista_dt_nasc[1];
+
+    $idade = $ano_atual - $ano_nasc;
+    
+    return $idade;
+}
---
+++ model/conexao.php
@@ -0,0 +1,13 @@
+<?php
+
+function conecta(){
+    
+    $host = 'localhost';
+    $user = 'root';
+    $pass = "";
+    $dbname = "escola";
+
+    $conexao = mysqli_connect($host, $user, $pass, $dbname);
+    return $conexao;
+
+}
