9159861c01899155f7ff7815b0d175b1ca392f4a
Bruno Queluci <brunoqueluci@gmail.com>
2022-04-20 17:36:06 -0300

    Ajuste de cálculo de idade por dia e mês.

--- controller/aluno.php
+++ controller/aluno.php
@@ -22,7 +22,7 @@ function cadastrar(){
     $id = insert($dados);
 
     echo '</br>';
-    echo '<a href="http://localhost/sistemaEscola/index.php" ><button class="btn btn-success">Voltar</button></a>';
+    echo '<a href="http://localhost/sistemaEscola/index.php"><button class="btn btn-success">Voltar</button></a>';
 }
 
 cadastrar();
@@ -30,18 +30,24 @@ cadastrar();
 function calcula_idade($data){
 
     $data_atual = date("Y-m-d");
-    $lista = explode('-', $data_atual);
-    
-    $ano_atual = $lista[0];
-    $mes_atual = $lista[1];
-
+    $lista_atual = explode('-', $data_atual);
     $lista_dt_nasc = explode('-', $data);
-    
+
+    $ano_atual = $lista_atual[0];
+    $mes_atual = $lista_atual[1];
+    $dia_atual = $lista_atual[2];
+
     $ano_nasc = $lista_dt_nasc[0];
     $mes_nasc = $lista_dt_nasc[1];
+    $dia_nasc = $lista_dt_nasc[2];
 
     $idade = $ano_atual - $ano_nasc;
     
+    if ($mes_atual < $mes_nasc){
+        $idade --;
+    } elseif ($dia_atual < $dia_nasc){
+        $idade --;
+    }
+
     return $idade;
 }
-
