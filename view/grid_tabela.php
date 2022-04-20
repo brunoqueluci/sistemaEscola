6d513e81904fef40c05f1bc7f631b336006e9488
Bruno Queluci <brunoqueluci@gmail.com>
2022-04-20 17:36:45 -0300

    ajuste de tabelas.

--- view/grid_tabela.php
+++ view/grid_tabela.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
 
   <div class="container">
-    <table class="table table-dark table-striped">
+    <table class="table table-dark table-striped mt-5">
       <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">ID</th>
+          <th scope="col">NOME</th>
+          <th scope="col">MATRÍCULA</th>
+          <th scope="col">NASCIMENTO</th>
+          <th scope="col">IDADE</th>
         </tr>
       </thead>
       <tbody>
@@ -24,16 +16,19 @@
           <td>Mark</td>
           <td>Otto</td>
           <td>@mdo</td>
+          <td>@mdo</td>
         </tr>
         <tr>
           <th scope="row">2</th>
           <td>Jacob</td>
           <td>Thornton</td>
           <td>@fat</td>
+          <td>@fat</td>
         </tr>
         <tr>
           <th scope="row">3</th>
           <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
           <td>@twitter</td>
         </tr>
       </tbody>
